c30cdb0a45c0da8786758298186a0823b9e0efde
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-11-15 00:48:34 +0600

    refactor: update layout and page structure, add new dependencies for authentication and prettier

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// import FooterSection from '@/components/shared/Footer';
+// import Navbar from '@/components/shared/Navbar';
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  return <div className="relative">{children}</div>;
+};
+
+export default Layout;
